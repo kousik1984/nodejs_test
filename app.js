5060c9e792645d07e0452afe09aafedca211bc8c
Kousik Das <53568703+kousik1984@users.noreply.github.com>
2024-10-07 23:32:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-// app.js
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Basic route to confirm app is working
 app.get('/', (req, res) => {
-  res.send('Hello, Docker!');
+  res.send('Hello from Node.js app deployed via AWS CodePipeline!');
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
-
